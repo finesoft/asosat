b3e2e28d14d08a99db3d4dbbd0edd8798ac5a4d2
GSFT-BINGOX\bingo <finesoft@gmail.com>
2018-09-07 11:28:23 +0800

    Introduce elastic mybatis

---
+++ asosat-elastic/src/main/java/org/asosat/elastic/mapping/annotation/EsIp.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013-2018. BIN.CHEN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/**
+ * @author bingo 下午8:57:42
+ *
+ */
+package org.asosat.elastic.mapping.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author bingo 2017年3月3日
+ * @since
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Inherited
+public @interface EsIp {
+
+  /**
+   * Mapping field-level query time boosting. Accepts a floating point number, defaults to 1.0.
+   *
+   * @return
+   */
+  float boost() default 1.0f;
+
+  /**
+   * Should the field be stored on disk in a column-stride fashion, so that it can later be used for
+   * sorting, aggregations, or scripting? Accepts true (default) or false.
+   *
+   * @return
+   */
+  boolean doc_values() default true;
+
+  /**
+   * Whether or not the field value should be included in the _all field? Accepts true or false.
+   * Defaults to false if index is set to false, or if a parent object field sets include_in_all to
+   * false. Otherwise defaults to true.
+   */
+  boolean include_in_all() default false;
+
+  /**
+   * Should the field be searchable? Accepts true (default) and false.
+   *
+   * @return
+   */
+  boolean index() default true;
+
+  /**
+   * Whether the field value should be stored and retrievable separately from the _source field.
+   * Accepts true or false (default).
+   *
+   * @return
+   */
+  boolean store() default false;
+
+}

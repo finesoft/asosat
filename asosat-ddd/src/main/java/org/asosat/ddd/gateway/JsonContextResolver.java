035954211990136e622ec096bbe761238117e40b
don <don43xu@qq.com>
2019-10-12 15:09:56 +0800

    json 处理

--- asosat-ddd/src/main/java/org/asosat/ddd/gateway/JsonContextResolver.java
+++ asosat-ddd/src/main/java/org/asosat/ddd/gateway/JsonContextResolver.java
@@ -13,6 +13,17 @@
  */
 package org.asosat.ddd.gateway;
 
+import static org.corant.shared.util.MapUtils.mapOf;
+import static org.corant.shared.util.ObjectUtils.defaultObject;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.corant.Corant;
+import org.corant.suites.bundle.EnumerationBundle;
+import org.corant.suites.json.JsonUtils;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Any;
@@ -23,8 +34,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
-import org.corant.suites.json.JsonUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * corant-asosat-ddd
@@ -37,7 +51,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Consumes(MediaType.APPLICATION_JSON)
 public class JsonContextResolver implements ContextResolver<ObjectMapper> {
 
-  private final ObjectMapper objectMapper = JsonUtils.copyMapper();
+  public final static Long BROWSER_SAFE_LONG = 9007199254740991L;
+  public final static BigInteger BROWSER_SAFE_BIGINTEGER = BigInteger.valueOf(BROWSER_SAFE_LONG);
+
+  private final static ObjectMapper objectMapper;
+  static {
+    SimpleModule simpleModule = new SimpleModule()
+            .addSerializer(new BigIntegerJsonSerializer())
+            .addSerializer(new LongJsonSerializer())
+            .addSerializer(new EnumJsonSerializer());
+    objectMapper = JsonUtils.copyMapper().registerModules(simpleModule);
+  }
 
   @Inject
   @Any
@@ -52,6 +76,91 @@ public class JsonContextResolver implements ContextResolver<ObjectMapper> {
   void onPostConstruct() {
     if (!configurator.isUnsatisfied()) {
       configurator.forEach(cfg -> cfg.config(objectMapper));
+    }
+  }
+
+  static final class LongJsonSerializer extends JsonSerializer<Long> {
+    @Override
+    public Class<Long> handledType() {
+      return Long.class;
+    }
+
+    @Override
+    public void serialize(Long value, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException {
+      if (value.compareTo(BROWSER_SAFE_LONG) > 0) {
+        gen.writeString(value.toString());
+      } else {
+        gen.writeNumber(value);
+      }
+    }
+  }
+
+  static final class BigIntegerJsonSerializer extends JsonSerializer<BigInteger> {
+    @Override
+    public Class<BigInteger> handledType() {
+      return BigInteger.class;
+    }
+
+    @Override
+    public void serialize(BigInteger value, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException {
+      if (value.compareTo(BROWSER_SAFE_BIGINTEGER) > 0) {
+        gen.writeString(value.toString());
+      } else {
+        gen.writeNumber(value);
+      }
+    }
+  }
+
+  @Deprecated
+  @SuppressWarnings("rawtypes")
+  static final class EnumJsonSerializer extends JsonSerializer<Enum> {
+
+    static final Map<Enum, Map<String, Object>> CACHES = new ConcurrentHashMap<>();
+
+    static volatile EnumerationBundle bundle;
+
+    @Override
+    public Class<Enum> handledType() {
+      return Enum.class;
+    }
+
+    @Override
+    public void serialize(Enum value, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException {
+      gen.writeObject(resolveEnumLiteral(value));
+    }
+
+    EnumerationBundle resolveBundle() {
+      if (bundle == null) {
+        synchronized (this) {
+          if (bundle == null && Corant.me() != null
+                  && Corant.instance().select(EnumerationBundle.class).isResolvable()) {
+            bundle = Corant.instance().select(EnumerationBundle.class).get();
+          } else {
+            bundle = new EnumJsonSerializerBundle();
+          }
+        }
+      }
+      return bundle;
+    }
+
+    Map<String, Object> resolveEnumLiteral(Enum value) {
+      return CACHES.computeIfAbsent(value, (v) -> {
+        String literal = resolveBundle().getEnumItemLiteral(value, Locale.getDefault());
+        return mapOf("name", value.name(), "literal", defaultObject(literal, value.name()), "class",
+                value.getDeclaringClass().getName(), "ordinal", value.ordinal());
+      });
+    }
+  }
+
+  @Deprecated
+  @SuppressWarnings("rawtypes")
+  static final class EnumJsonSerializerBundle implements EnumerationBundle {
+    @Override
+    public String getEnumItemLiteral(Enum enumVal, Locale locale) {
+      return enumVal.name();
     }
   }
 }

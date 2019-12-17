20c66fffeb8df1aeb70d93c702f1a83f8055bc29
don <don@gixon.com>
2019-12-17 14:23:59 +0800

    numbergeneration 返回string

--- asosat-ddd/src/main/java/org/asosat/ddd/service/NumberGeneration.java
+++ asosat-ddd/src/main/java/org/asosat/ddd/service/NumberGeneration.java
@@ -1,7 +1,7 @@
 package org.asosat.ddd.service;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -31,25 +31,23 @@ public class NumberGeneration {
 
   private static final String PREFIX_KEY = NumberGeneration.class.getName() + ":";
 
-  private static final Logger LOG = Logger.getLogger(NumberGeneration.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(NumberGeneration.class.getName());
 
   @Inject
   RedissonClient redisson;
 
-  public Long nextUniqueNoOfDay(int businessCode, int initTailDigit) {
+  public String nextUniqueNoOfDay(int businessCode, int initTailDigit) {
     ZonedDateTime now = ZonedDateTime.now();
     final String today = FORMATTER.format(now);
     final String dataKey = PREFIX_KEY + "data_" + businessCode + "_" + today; //e.g. className:data_1_191216
     final String digitKey = PREFIX_KEY + "digit_" + businessCode + "_" + today; //e.g. className:digit_1_191216
-    LOG.fine("dataKey   " + dataKey);
-    LOG.fine("digitKey  " + digitKey);
     RBlockingQueue<Integer> blockingQueue = redisson.getBlockingQueue(dataKey);
     if (blockingQueue.isEmpty()) {
       RLock rLock = redisson.getLock(digitKey + "_lock");//e.g. className:digit_1_191216_lock
       rLock.lock();
       try {
         if (blockingQueue.isEmpty()) {
-          LOG.fine("blockingQueue is empty");
+          LOGGER.fine(() -> "blockingQueue is empty");
           RAtomicLong atomicLong = redisson.getAtomicLong(digitKey);
           int digit;
           if (atomicLong.isExists()) {
@@ -64,16 +62,16 @@ public class NumberGeneration {
           Collections.shuffle(data);
           blockingQueue.addAll(data);
 
-          ZonedDateTime tomorrow = now.plusDays(1).with(t -> t.with(SECOND_OF_DAY, 1));
+          ZonedDateTime tomorrow = now.plusDays(1).with(t -> t.with(HOUR_OF_DAY, 1));
           atomicLong.expireAt(tomorrow.toInstant().toEpochMilli());
           blockingQueue.expireAt(tomorrow.toInstant().toEpochMilli());
-          LOG.fine("blockingQueue is done");
+          LOGGER.fine(() -> "blockingQueue is done");
         }
       } finally {
         rLock.unlock();
       }
     }
     Integer currentNo = blockingQueue.remove();
-    return Long.valueOf(businessCode + today + currentNo);
+    return businessCode + today + currentNo;
   }
 }
